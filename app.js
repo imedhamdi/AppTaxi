f2a888db96d82e1c72eda4e3a8efbd55f8270582
Imed HAMDI <imedhamdi007@hotmail.fr>
2022-09-08 21:25:05 +0200

    Css + Navbar

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fetch = require('node-fetch');
 var cors = require('cors');
+var morgan = require('morgan')
 const app = express();
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -8,9 +9,10 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
-
+app.disable('x-powered-by');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(express.static('public'));
+app.use(morgan('tiny'));
 module.exports = app;
